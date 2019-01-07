208ab2aeb3588be50f0b556b29da700819617882
Leon Do <leondo123@gmail.com>
2019-01-07 15:28:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-/*
-server
-9cf492dcd4a1724470181fcfeff833710eec58fd6a4e926a8b760266dfde9659
-TsX1TGWi6Ss6CPNz7kcGSq5be7Q1ogVyerK
-
-client
-f91b705c29978d7f5472201129f3edac61da67e4e2ec9dde1f6b989582321dbf
-TsRDtJmAbavWHUEaDcCjG7YwDRJThhAnafp
-*/
-
 const bitcore = require('bitcore-lib')
-var explorers = require('bitcore-explorers');
+const explorers = require('bitcore-explorers');
 const insight = new explorers.Insight('https://testnet.decred.org'); // https://mainnet.decred.org/
 const network = bitcore.Networks.dcrdtestnet // dcrdlivenet
-
-// generate server pub/priv key
-const serverPrivateKey = new bitcore.PrivateKey('9cf492dcd4a1724470181fcfeff833710eec58fd6a4e926a8b760266dfde9659', network);
-const serverPublicKey = bitcore.PublicKey(serverPrivateKey);
-const serverAddress = serverPublicKey.toAddress(network) 
 
 // generate client pub/priv key
 const clientPrivateKey = new bitcore.PrivateKey('f91b705c29978d7f5472201129f3edac61da67e4e2ec9dde1f6b989582321dbf', network)
